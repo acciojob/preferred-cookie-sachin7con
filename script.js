5fa718d9c3cf21d9cf73704bc094aec864395e5a
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-10 17:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //SGN your JS code here. If required.
-document.addEventListener('DOMContentLoaded', () => {
+In your HTML, the submit button does not have an id, but in your JavaScript, you’re trying to get the button by its id. This is why the error is occurring.
+
+To fix this, you need to add an id to your submit button in the HTML. Here’s how you can do it:
+
+<input type="submit" id="submit" value="Save" />
+Also, it seems you are trying to change CSS variables using JavaScript. To achieve this, you need to use the setProperty method. Here’s how you can modify your JavaScript code:
+
+window.onload = function() {
     const submit = document.getElementById("submit");
 
-    submit.addEventListener('click', () => {
-        const fontSize = document.getElementById("fontsize").value;
-        const fontColor = document.getElementById("fontcolor").value;
-        document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
-        document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
-    });
-});
+    if(submit) {
+        submit.addEventListener('click', (event) => {
+            event.preventDefault(); // to prevent form submission
+            const fontSize = document.getElementById("fontsize").value;
+            const fontColor = document.getElementById("fontcolor").value;
+            document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
+            document.documentElement.style.setProperty('--fontcolor', fontColor);
+            document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
+            document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
+        });
+    } else {
+        console.error('Submit button not found');
+    }
+};
