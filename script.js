afa510061fc03efc7ed575b707add849d660a2f7
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-10 17:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const submit = document.getElementById("submit");
 
 submit.addEventListener('click', () => {
 	const fontSize = document.getElementById("fontsize").value;
-	const fontcolor = document.getElementById("fontcolor").value;
+	const fontColor = document.getElementById("fontcolor").value;
 	document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
 	document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
 	
