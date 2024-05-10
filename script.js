0b493eea17eb03869f096cf5960e008fc31e2d08
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-10 17:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //SGN your JS code here. If required.
-const submit = document.getElementById("submit");
+document.addEventListener('DOMContentLoaded', () => {
+    const submit = document.getElementById("submit");
 
-
-submit.addEventListener('click', () => {
-	const fontSize = document.getElementById("fontsize").value;
-	const fontColor = document.getElementById("fontcolor").value;
-	document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
-	document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
-	
-})
+    submit.addEventListener('click', () => {
+        const fontSize = document.getElementById("fontsize").value;
+        const fontColor = document.getElementById("fontcolor").value;
+        document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
+        document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
+    });
+});
