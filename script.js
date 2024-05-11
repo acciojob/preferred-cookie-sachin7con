303f43ed1a0d6eea721a598f201484ce4150908f
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-11 12:00:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ window.onload = function() {
             const fontColor = document.getElementById("fontcolor").value;
             document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
             document.documentElement.style.setProperty('--fontcolor', fontColor);
-            document.cookie = `fontSize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
-            document.cookie = `fontColor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
+            document.cookie = `fontsize=${fontSize}; expires=Sat, 11 May 2024 17:30:00 GMT`;
+            document.cookie = `fontcolor=${fontColor}; expires=Sat, 11 May 2024 17:35:00 GMT`;
         });
     } else {
         console.error('Submit button not found');
